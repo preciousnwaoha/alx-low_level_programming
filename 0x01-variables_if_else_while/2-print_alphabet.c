928783f7d1f8b4e4c6bf37748fd2c90ec3816860
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-06-07 04:53:47 +0000

    All tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 0; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
