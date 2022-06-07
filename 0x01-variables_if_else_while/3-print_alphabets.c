e4efd1680a5651a7afa75a5b7148f580edefad80
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-06-07 06:44:31 +0000

    Tasks Correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,17 +5,17 @@
  *
  * Return: Always 0.
  */
- int main(void)
- {
-	 char letter;
+int main(void)
+{
+	char letter;
 
-	 for (letter = 'a'; letter <= 'z'; letter++)
-		 putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-	 for (letter = 'A'; letter <= 'Z'; letter++)
-		 putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
-	 putchar('\n');
+	putchar('\n');
 
-	 return (0);
- }
+	return (0);
+}
