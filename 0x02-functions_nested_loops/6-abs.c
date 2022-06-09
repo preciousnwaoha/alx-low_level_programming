0d6752ded786aaa01f1d00e27c5ec6a5be0473c0
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-06-09 15:59:40 +0000

    Task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - prints absolute value of number
+ * @n: The number in question
+ *
+ * Return: Always 0 (Success)
+ */
+ int _abs(int n)
+ {
+	 _putchar(abs(n));
+	 return (0);
+ }
