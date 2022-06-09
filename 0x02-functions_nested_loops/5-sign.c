4b2f4a9101141c55715feb3111cdcc1a237426ad
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-06-09 15:41:14 +0000

    Task 5 done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: number in question
+ *
+ * Return: 1 if n is positive, 0 if n is zero and -1 if n is negative
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar("1");
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
