7f26fd905fe699ab170a1592868227b9dd3743b5
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-06-09 23:21:21 +0000

    Times table Task 9 correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (col = 0; col <= 9; col++)
+		for (col = 1; col <= 9; col++)
 		{
 			d = (row * col);
 			if ((d / 10) > 0)
